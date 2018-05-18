b893affc354bf80acc6b7da691efeabef2fd777e
gielaagiel <gielaagiel@gmail.com>
2018-05-18 22:49:01 +0700

    library tweet

---
+++ src/jcreator_test.java
@@ -0,0 +1,17 @@
+/**
+ * @(#)jcreator_test.java
+ *
+ * jcreator_test application
+ *
+ * @author 
+ * @version 1.00 2018/5/18
+ */
+ 
+public class jcreator_test {
+    
+    public static void main(String[] args) {
+    	
+    	// TODO, add your application code
+    	System.out.println("Hello World!");
+    }
+}
